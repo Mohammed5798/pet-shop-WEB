9c6e633aef056fae63f2e17011ed4b1607d5fba6
unknown <mohammedrich000@gmail.com>
2022-12-06 16:40:48 +0300

    last update

--- dog.js
+++ dog.js
@@ -1,77 +1,35 @@
-let form = document.querySelector('.header .login-from');
-
-var loginBtn = document.querySelector("#login-btn");
-
-loginBtn.onclick =  () => {
-
-    form.classList.toggle("active");
-
-    navbar.classList.remove("active");
-
-    tallk.classList.remove("active");
-
-    btno.classList.remove("active");
-
-    btnn.classList.remove("active");
-
+var _a, _b, _c, _d;
+//*--------------------scroll--------------------
+window.onscroll = function () {
+    var _a, _b;
+    if (window.scrollY > 0) {
+        (_a = document.querySelector(".header")) === null || _a === void 0 ? void 0 : _a.classList.add("active");
+        page === null || page === void 0 ? void 0 : page.classList.remove("active");
+    }
+    else {
+        (_b = document.querySelector(".header")) === null || _b === void 0 ? void 0 : _b.classList.remove("active");
+    }
+    ;
 };
-
-
-const navbar = document.querySelector(".header .navbar");
-
-const button = document.querySelector(".header .icons #menu").onclick = () => {
-    
-    navbar.classList.toggle("active");
-
-    form.classList.remove("active");
-};
-
-window.onscroll = () => {
-    
-    navbar.classList.remove("active");
-
-    form.classList.remove("active");
-
-
-    if(window.scrollY > 0){
-        document.querySelector(".header").classList.add("active");
-    }else{
-        document.querySelector(".header").classList.remove("active");
-    };
-}
-
-// window.onload = () => {
-
-//     if(window.scrollY > 0){
-//         document.querySelector(".header").classList.add("active");
-//     }
-//     else {
-//         document.querySelector(".header").classList.remove("active")
-//     };
-// };
-
-const btnn = document.querySelector(".about .content .btnn");
-
-const btno = document.querySelector(".about .content .btno");
-
-const tallk = document.querySelector(".about .content .parg");
-
-btnn.onclick = () =>{
-    
-    tallk.classList.add("active");
-
-    btno.classList.toggle("active");
-
-    btnn.classList.toggle("active");
-
-    form.classList.remove("active");
-};
-
-btno.onclick = () =>{
-    
-    tallk.classList.remove("active");
-    
-    btno.classList.toggle("active");
-    
-    btnn.classList.toggle("active");
-};
+//*--------------display-page----------------
+var button = (_a = document.querySelector(".header .icons #login-btn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+    page === null || page === void 0 ? void 0 : page.classList.toggle("active");
+    navbar === null || navbar === void 0 ? void 0 : navbar.classList.remove("active");
+});
+var page = document.querySelector(".header .login-from");
+//*--------------read-more-------------------
+var parg = document.querySelector(".about .content .parg");
+var btnn = (_b = document.querySelector(".about .content .btnn")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", function () {
+    parg === null || parg === void 0 ? void 0 : parg.classList.add("active");
+    btno === null || btno === void 0 ? void 0 : btno.classList.add("active");
+    //btnn?.classList.remove("active")
+});
+var btno = (_c = document.querySelector(".about .content .btno")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", function () {
+    parg === null || parg === void 0 ? void 0 : parg.classList.remove("active");
+});
+//*-------------------navbar-----------------
+var navbar = document.querySelector(".header .navbar");
+var menu = (_d = document.querySelector(".header .icons #menu")) === null || _d === void 0 ? void 0 : _d.addEventListener("click", function () {
+    navbar === null || navbar === void 0 ? void 0 : navbar.classList.toggle("active");
+    page === null || page === void 0 ? void 0 : page.classList.remove("active");
+});
--- dog.ts
+++ dog.ts
@@ -1,9 +1,51 @@
-let form = document.querySelector('header login-from') as HTMLFormElement;
+//*--------------------scroll--------------------
+window.onscroll = () => {
 
-let loginBtn =  document.querySelector("#login-btn") as HTMLSelectElement;
+    if(window.scrollY >0){
+        document.querySelector(".header")?.classList.add("active");
 
-loginBtn.onclick = () => {
-
-    form.classList.toggle("active");
-    
+        page?.classList.remove("active");
+    }else{
+        document.querySelector(".header")?.classList.remove("active");
+    };
 };
+
+//*--------------display-page----------------
+
+let button = document.querySelector(".header .icons #login-btn")?.addEventListener("click" , ()=>{
+
+    page?.classList.toggle("active");
+
+    navbar?.classList.remove("active");
+})
+
+let page = document.querySelector(".header .login-from")!;
+
+//*--------------read-more-------------------
+let parg = document.querySelector(".about .content .parg")!;
+
+let btnn = document.querySelector(".about .content .btnn")?.addEventListener("click" , () => {
+
+    parg?.classList.add("active");
+
+    btno?.classList.add("active");
+
+    //btnn?.classList.remove("active")
+})
+
+let btno = document.querySelector(".about .content .btno")?.addEventListener("click" , () => {
+
+    parg?.classList.remove("active");
+})
+
+
+//*-------------------navbar-----------------
+
+let navbar = document.querySelector(".header .navbar");
+
+let menu = document.querySelector(".header .icons #menu")?.addEventListener("click" , () => {
+
+    navbar?.classList.toggle("active");
+
+    page?.classList.remove("active");
+})
